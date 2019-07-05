184ba1b5f65be157f50a9e75c2ed7cdf3d5c9568
Alexander Wayne Omorokunwa <xeroxism@gmail.com>
2019-07-05 12:31:34 +0100

    updated to the latest API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function convertCurrency() {
+
     //collect varaibles(inputs) from the form
     var from = document.getElementById('from').value;
     var to = document.getElementById('to').value;
@@ -7,25 +7,18 @@ function convertCurrency() {
     var currId = from + "_" + to;
 
 
-    //request rate from the API and do the currency conversion
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    fetch(`https://free.currconv.com/api/v6/convert?q=${currId}&compact=ultra&apiKey=4e89e9e6ebef7956885e`)
+        .then(res => res.json())
+        .then(data => {
 
-            //assign the object returned to a variable
-            var myObj = JSON.parse(this.responseText);
+                /* select the first value as rate*/
 
-            //select the rate value from the JSON object
-            firstValue = Object.values(myObj);
-           let rate = firstValue[0];
-            //convert to the amount entered
-           let convertedAmount = rate * amount;
-                //output the convertedAmount to the DOM
-            result.innerHTML = convertedAmount.toFixed(2);
-        }
-    };
-    xmlhttp.open("GET", `https://free.currconv.com/api/v7/convert?q=${currId}&compact=ultra&apiKey=4e89e9e6ebef7956885e`, true);
-    xmlhttp.send();
+                var rate = Object.values(data)[0];
+                /*convert the amount with the rate*/
+                var convertedAmount = amount * rate;
+                /*display the result to the DOM*/
+                result.innerHTML = convertedAmount.toFixed(2);
+            }
 
+        );
 }
-
